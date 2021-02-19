213b9d9dbe13712c6e8041c54473906b0f3a56fe
Daniel Listyo <danielistyo16@gmail.com>
2021-02-19 17:57:28 +0700

    Add Sidebar component

---
+++ src/components/MainSidebar/MainSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <prime-sidebar visible position="left"></prime-sidebar>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import PrimeSidebar from 'primevue/sidebar';
+
+export default defineComponent({
+  name: 'MainSidebar',
+  components: {
+    PrimeSidebar,
+  },
+});
+</script>
+
+<style src="./MainSidebar.scss" lang="scss" scoped></style>
