2a6ab8b5d3cd2b9993fad6c0f7c0f4fac1f8120c
Daniel Listyo <danielistyo16@gmail.com>
2021-02-19 17:57:05 +0700

    Add header component

---
+++ src/components/MainHeader/MainHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-header">
+    <i class="pi pi-bars main-header__toggle-sidebar" @click="showSidebar"></i>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import emitter from '@/emitter';
+
+export default defineComponent({
+  name: 'MainHeader',
+  setup() {
+    const showSidebar = () => {
+      emitter.emit('sidebar:show', true);
+    };
+    return { showSidebar };
+  },
+});
+</script>
+
+<style src="./MainHeader.scss" lang="scss" scoped></style>
