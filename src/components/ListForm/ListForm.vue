adf166102e3d9faa817e4b51805300904a7e2efe
Daniel <danielistyo16@gmail.com>
2021-04-08 01:02:53 +0700

    add pagination in ListForm

--- src/components/ListForm/ListForm.vue
+++ src/components/ListForm/ListForm.vue
@@ -9,7 +9,7 @@
       />
     </div>
     <card
-      v-for="(unit, index) in list"
+      v-for="(unit, index) in paginatedList"
       :key="index"
       @click="$emit('selected', unit.key)"
       class="p-mb-2 unit-list"
@@ -19,25 +19,36 @@
           <div class="unit-list__number">{{ unit.nomor_program }}</div>
           <div class="unit-list__name">{{ unit.nama_program }}</div>
           <div class="unit-list__created">
-            <i class="pi pi-calendar" /> {{ convertToDate(unit.created_at) }}
+            <i class="pi pi-calendar" />
+            {{ convertToDate(unit.created_at) }}
           </div>
           <i class="unit-list__icon-right pi pi-angle-right" />
         </div>
       </template>
     </card>
+    <paginator
+      @page="handlePageChange"
+      :totalRecords="list.length"
+      :rows="5"
+      template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import ButtonPrime from 'primevue/button';
+import Paginator from 'primevue/paginator';
 import Card from 'primevue/card';
 import dayjs from 'dayjs';
+import { ref, unref } from '@vue/reactivity';
+import { computed, defineComponent } from '@vue/runtime-core';
 
-export default {
+export default defineComponent({
   name: 'ListForm',
   components: {
     Card,
     ButtonPrime,
+    Paginator,
   },
   emits: ['selected', 'addclick'],
   props: {
@@ -46,13 +57,28 @@ export default {
       required: true,
     },
   },
-  setup() {
+  setup(props) {
     const convertToDate = (timestamp: number) => {
-      return dayjs.unix(timestamp).format('DD-MM-YYYY HH:ss');
+      return dayjs.unix(timestamp).format('DD-MM-YYYY HH:mm');
     };
-    return { convertToDate };
+
+    const page = ref(0);
+    const paginatedList = computed(() => {
+      return props.list.slice(unref(page), unref(page) + 5);
+    });
+
+    const handlePageChange = ($event: {
+      page: number;
+      first: number;
+      rows: number;
+      pageCount: number;
+    }) => {
+      page.value = $event.page;
+    };
+
+    return { convertToDate, handlePageChange, paginatedList };
   },
-};
+});
 </script>
 
 <style src="./ListForm.scss" lang="scss" scoped></style>
